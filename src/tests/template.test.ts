b50e6917a64d1da038d9d3d0b1045ef8e7024279
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-01-28 11:07:31 +0100

    Red: should return an error when the template engine is instantiated with empty content

--- src/tests/template.test.ts
+++ src/tests/template.test.ts
@@ -1,10 +1,30 @@
-import { Template } from '../core/template';
+/*
+- should return an error when the template engine is instantiated with empty content
+- should return the template content when no variables included
+- should return the template content with one variable replaced
+- should return the template content with two variables replaced
+- should return error when a variable is empty string
+- should return error when a variable is null
+*/
+
+import { TemplateEngine } from '../core/template';
 
 describe('The Template Engine', () => {
 	it('should be instantiated.', () => {
-		const template = Template.create();
+		const templateContent = "";
+		
+		const template = TemplateEngine.create(templateContent);
 
-		expect(template).toBeInstanceOf(Template);
+		expect(template).toBeInstanceOf(TemplateEngine);
+	});
+
+	it('should return an error when the template engine is instantiated with empty content.', () => {
+		const templateContent = "";
+
+		expect(()=>{
+			TemplateEngine.create(templateContent);
+		}).toThrow();
+		
 	});
 });
 
